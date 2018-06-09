23f44d69578baa44f3a1dc4ba4f5bcd378deeab7
markpattison <mr.pattison@gmail.com>
2018-06-09 14:50:03 +0100

    removing FAKE dotnet warnings

--- build.fsx
+++ build.fsx
@@ -1,6 +1,12 @@
 // include Fake libs
 #r "packages/FAKE/tools/FakeLib.dll"
 
+// TODO: once migrated to FAKE 5 proper, remove this plus paket references to Newtonsoft.Json and GitHub file
+#r "packages/Newtonsoft.Json/lib/netstandard2.0/Newtonsoft.Json.dll"
+open Newtonsoft.Json.Linq
+#load "paket-files/fsharp/FAKE/src/app/Fake.DotNet.Cli/DotNet.fs"
+//
+
 open Fake
 open Fake.Core.TargetOperators
 open Fake.IO.Globbing.Operators
@@ -12,23 +18,20 @@ let fableReferences =
 let fableDirectory = "src"
 
 let dotnetcliVersion = "2.1.201"
-let mutable dotnetExePath = "dotnet"
+
+let install = lazy Fake.DotNet.DotNet.install (fun p -> { p with Version = Fake.DotNet.DotNet.CliVersion.Version dotnetcliVersion })
 
 // Targets
-Fake.Core.Target.create "InstallDotNetCore" (fun _ ->
-    dotnetExePath <- DotNetCli.InstallDotNetSDK dotnetcliVersion
-)
-
 Fake.Core.Target.create "Clean" (fun _ ->
     [ fableReferences ]
     |> Seq.concat
-    |> Seq.iter (fun proj -> DotNetCli.RunCommand id ("clean " + proj))
+    |> Seq.iter (fun proj -> Fake.DotNet.DotNet.exec id "clean" proj |> ignore)
 )
 
 Fake.Core.Target.create "Restore" (fun _ ->
     [ fableReferences ]
     |> Seq.concat
-    |> Seq.iter (fun proj -> DotNetCli.Restore (fun p -> { p with Project = proj; ToolPath = dotnetExePath; AdditionalArgs = [ "--no-dependencies" ] }))
+    |> Seq.iter (fun proj -> Fake.DotNet.DotNet.restore id proj)
 )
 
 Fake.Core.Target.create "NpmInstall" (fun _ ->
@@ -38,18 +41,21 @@ Fake.Core.Target.create "NpmInstall" (fun _ ->
 
 Fake.Core.Target.create "BuildFable" (fun _ ->
     fableReferences
-    |> Seq.iter (fun proj -> DotNetCli.RunCommand (fun p -> { p with WorkingDir = fableDirectory; ToolPath = dotnetExePath }) ("fable npm-build " + proj))
+    |> Seq.iter (fun proj ->
+        Fake.DotNet.DotNet.exec (fun p ->
+            { p with WorkingDirectory = fableDirectory }) "fable npm-build" proj |> ignore)
 )
 
 Fake.Core.Target.create "RunFable" (fun _ ->
     fableReferences
-    |> Seq.iter (fun proj -> DotNetCli.RunCommand (fun p -> { p with WorkingDir = fableDirectory; ToolPath = dotnetExePath }) ("fable npm-start " + proj))
+    |> Seq.iter (fun proj ->
+        Fake.DotNet.DotNet.exec (fun p ->
+            { p with WorkingDirectory = fableDirectory }) "fable npm-start " proj |> ignore)
 )
 
 // Build order
-"InstallDotNetCore"
-    ==> "Clean"
-//    ==> "UpdateVersionNumber"
+
+"Clean"
     ==> "Restore"
     ==> "NpmInstall"
 
