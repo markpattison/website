76c84625839b4c3dbdbf267a69ed489651b27c5c
markpattison <mr.pattison@gmail.com>
2018-06-11 09:26:37 +0100

    upgarde to FAKE 5 proper

--- build.fsx
+++ build.fsx
@@ -1,15 +1,9 @@
-// include Fake libs
-#r "packages/FAKE/tools/FakeLib.dll"
+#r "paket: groupref Build //"
+#load "./.fake/build.fsx/intellisense.fsx"
 
-// TODO: once migrated to FAKE 5 proper, remove this plus paket references to Newtonsoft.Json and GitHub file
-#r "packages/Newtonsoft.Json/lib/netstandard2.0/Newtonsoft.Json.dll"
-open Newtonsoft.Json.Linq
-#load "paket-files/fsharp/FAKE/src/app/Fake.DotNet.Cli/DotNet.fs"
-//
-
-open Fake
-open Fake.Core.TargetOperators
+open Fake.Core
 open Fake.IO.Globbing.Operators
+open Fake.DotNet
 
 // Filesets
 let fableReferences = 
@@ -17,43 +11,56 @@ let fableReferences =
 
 let fableDirectory = "src"
 
-let dotnetcliVersion = "2.1.201"
+let dotnetcliVersion = "2.1.300"
 
-let install = lazy Fake.DotNet.DotNet.install (fun p -> { p with Version = Fake.DotNet.DotNet.CliVersion.Version dotnetcliVersion })
+let install = lazy DotNet.install (fun p ->
+    { p with Version = DotNet.CliVersion.Version dotnetcliVersion })
+
+let inline dotnet arg = DotNet.Options.lift install.Value arg
+
+let inline withWorkDir wd =
+    DotNet.Options.lift install.Value
+    >> DotNet.Options.withWorkingDirectory wd
 
 // Targets
-Fake.Core.Target.create "Clean" (fun _ ->
+Target.description "Cleaning directories"
+Target.create "Clean" (fun _ ->
     [ fableReferences ]
     |> Seq.concat
-    |> Seq.iter (fun proj -> Fake.DotNet.DotNet.exec id "clean" proj |> ignore)
+    |> Seq.iter (fun proj ->
+        let result = DotNet.exec dotnet "clean" proj
+        if not result.OK then failwithf "dotnet clean failed with code %i" result.ExitCode)
 )
 
-Fake.Core.Target.create "Restore" (fun _ ->
+Target.description "Restoring .NET dependencies"
+Target.create "Restore" (fun _ ->
     [ fableReferences ]
     |> Seq.concat
-    |> Seq.iter (fun proj -> Fake.DotNet.DotNet.restore id proj)
+    |> Seq.iter (fun proj -> DotNet.restore dotnet proj)
 )
 
-Fake.Core.Target.create "NpmInstall" (fun _ ->
-    Fake.JavaScript.Npm.install (fun p ->
-        { p with WorkingDirectory = fableDirectory })
+Target.description "Restoring Node dependencies"
+Target.create "NpmInstall" (fun _ ->
+    Fake.JavaScript.Npm.install (fun p -> { p with WorkingDirectory = fableDirectory })
 )
 
-Fake.Core.Target.create "BuildFable" (fun _ ->
+Target.description "Building Fable for production"
+Target.create "BuildFable" (fun _ ->
     fableReferences
     |> Seq.iter (fun proj ->
-        Fake.DotNet.DotNet.exec (fun p ->
-            { p with WorkingDirectory = fableDirectory }) "fable npm-build" proj |> ignore)
+        DotNet.exec (withWorkDir fableDirectory) "fable npm-build" proj |> ignore)
 )
 
-Fake.Core.Target.create "RunFable" (fun _ ->
+Target.description "Building Fable for local run"
+Target.create "RunFable" (fun _ ->
     fableReferences
     |> Seq.iter (fun proj ->
-        Fake.DotNet.DotNet.exec (fun p ->
-            { p with WorkingDirectory = fableDirectory }) "fable npm-start " proj |> ignore)
+        DotNet.exec (withWorkDir fableDirectory) "fable npm-start " proj |> ignore)
 )
 
 // Build order
+
+open Fake.Core.TargetOperators
 
 "Clean"
     ==> "Restore"
@@ -62,5 +69,6 @@ Fake.Core.Target.create "RunFable" (fun _ ->
 "NpmInstall" ==> "BuildFable"
 "NpmInstall" ==> "RunFable"
 
-// start build
-Fake.Core.Target.runOrDefault "BuildFable"
+// Start build
+
+Target.runOrDefault "BuildFable"
